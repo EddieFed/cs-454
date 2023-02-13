a4b829002085818d45d5b5e5e34c6eaf8ba369b7
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2023-02-12 20:55:35 -0600

    Philosopher Wait/Notify Implimented

---
+++ DiningPhilosiphers/src/PhilosopherWaitNotify/Main.java
@@ -0,0 +1,31 @@
+package PhilosopherWaitNotify;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Philosopher> philosophers = new ArrayList<>();
+        ArrayList<Chopstick> chopsticks = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+           chopsticks.add(new Chopstick());
+        }
+
+        philosophers.add(new Philosopher(chopsticks.get(4), chopsticks.get(0), 0));
+        for (int i = 1; i < 5; i++) {
+            philosophers.add(new Philosopher(chopsticks.get(i - 1), chopsticks.get(i), i));
+        }
+
+        for (Philosopher p : philosophers) {
+            p.start();  // Start all philosophers!
+        }
+
+        for (Philosopher p : philosophers) {
+            p.join();   // Wait for all threads to complete!
+                        // for the sake of debugging and testing!
+        }
+
+        System.out.println("All philosophers have eaten!");
+
+    }
+}
---
+++ DiningPhilosiphers/src/PhilosopherWaitNotify/Philosopher.java
@@ -0,0 +1,46 @@
+package PhilosopherWaitNotify;
+
+import java.util.Random;
+
+public class Philosopher extends Thread {
+
+    Chopstick leftChopstick;
+    Chopstick rightChopstick;
+//    boolean hasEaten;
+    int pID;
+
+    public Philosopher(Chopstick left, Chopstick right, int pID) {
+        this.leftChopstick = left;
+        this.rightChopstick = right;
+//        this.hasEaten = true;
+        this.pID = pID;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();   // Since the PhilosopherWaitNotify.Philosopher "thinks" before eating
+                                        // we can simulate this with a rand int
+        while (true) {
+            try {
+                Thread.sleep(random.nextInt(10000));  // PhilosopherWaitNotify.Philosopher "thinks"
+
+                System.out.println("PhilosopherWaitNotify.Philosopher #" + pID + " is hungry, attempting to eat!");
+
+                // This is susceptible to deadlock since everyone picks up the same chopstick first, imagine if everyone
+                // picked the left one up and now was stuck waiting for the right to become available. Nobody wins...
+                leftChopstick.pickUp();
+                rightChopstick.pickUp();
+
+                System.out.println("PhilosopherWaitNotify.Philosopher #" + pID + " finished eating!");
+
+                leftChopstick.putBack();
+                rightChopstick.putBack();
+
+                return;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
